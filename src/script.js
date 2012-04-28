f888832edf892ffcca7512ce675a77026bfea025
Rémi Prévost <remi@exomel.com>
2012-04-28 15:09:03 -0400

    Use GitHub’s new anchors

--- src/script.js
+++ src/script.js
@@ -60,42 +60,20 @@ window.Lengthy = {
 
   populateToc: function() { // {{{
     // Most of this method code was extracted from @rafBM’s excellent <https://github.com/rafBM/heading-anchors> library
-    var slugize = function(str) { return str.replace(/['’]/g, '').replace(/[^a-z0-9]+/ig, '-') }
-    var existingSlugsNumbers = {}
-      , selector = ".markdown-body h1, .markdown-body h2, .markdown-body h3, .markdown-body h4, .markdown-body h5, .markdown-body h6"
+    var selector = ".markdown-body h1, .markdown-body h2, .markdown-body h3, .markdown-body h4, .markdown-body h5, .markdown-body h6"
 
     this.$(selector).forEach(function(heading) {
-      var anchor = document.createElement('a')
-        , item = document.createElement('li')
-        , slug = slugize(heading.id ? heading.id : heading.textContent)
+      var item = document.createElement('li')
         , title = heading.textContent
         , level = parseInt(heading.tagName.replace(/\w/, ""))
-
-      // if slug #Foo-bar already exists, create #Foo-bar-2, #Foo-bar-3 and so on
-      if (slug in existingSlugsNumbers) {
-        existingSlugsNumbers[slug] += 1
-        slug += '-' + existingSlugsNumbers[slug]
-      } else {
-        existingSlugsNumbers[slug] = 1
-      }
-
-      // Add the anchor element to the heading
-      anchor.href = "#"+slug
-      anchor.innerHTML = " ¶"
-      anchor.className = "heading-anchor"
-      heading.id = slug
-      heading.appendChild(anchor)
+        , anchor = heading.querySelectorAll(".anchor")[0]
 
       // Add the link in the table of contents
       var toc = document.getElementById("lengthy-toc")
-      item.innerHTML = "<a href='#"+slug+"'>" + title + "</a>"
+      item.innerHTML = "<a href='#"+anchor.getAttribute("name")+"'>" + title + "</a>"
       item.style.marginLeft = (5 + (level - 1) * 20)+"px"
       toc.appendChild(item)
     })
-
-    var headingInHash = document.getElementById(location.hash.substr(1))
-    if (headingInHash)
-      window.scrollTo(0, headingInHash.offsetTop)
   } // }}}
 
 }
